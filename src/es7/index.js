d1621bb41ea7c40bb1f20b8638998a80bd968365
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-02-21 11:02:19 -0600

    Clase 6

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+//INCLUDES
+let numbers = [1,2,3,7,8,]
+if(numbers.includes(9)){
+  console.log('Si se ecuentra 7')
+} else{
+  console.log('No se encuentra el 7')
+}
+
+//EXPONENTES
+let base = 4
+let exponente = 4
+let result = base**exponente
+console.log(result)
