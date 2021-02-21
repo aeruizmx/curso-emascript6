2b4f8c1b24e44dc0454a2b3aeea0c6da197d8c57
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-02-21 11:19:08 -0600

    Clase 8

--- src/es8/index.js
+++ src/es8/index.js
@@ -24,3 +24,27 @@ const string = 'hello'
 console.log(string.padStart(70,'hi'))
 console.log(string.padEnd(123,'-'))
 console.log('food'.padEnd(12, ' _'))
+
+//ASYNC Y AWAIT
+const helloWorld = () => {
+  return new Promise((resolve, reject)=>{
+    (true)
+      ? setTimeout(()=>resolve('Hello world'), 3000)
+      : reject(new Error('Test error'))
+  })
+}
+const helloAsync = async() =>{
+  const hello = await helloWorld()
+  console.log(hello)
+}
+
+helloAsync()
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld()
+    console.log(hello)
+  } catch (error) {
+    console.log(error)
+  }
+}
